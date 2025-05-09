796ad6f2c003feef1519ad5b218b4f6311b515d8
AmonKissi <amondkissi@gmail.com>
2025-05-09 11:46:58 -0500

    update path

--- version-3/app/page.tsx
+++ version-3/app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
               }}
             >
               <Link
-                href="https://app.brstore.us/plan"
+                href="https://app.buildandrun.netx/plan"
                 target="_blank"
                 className="group rounded-full pl-[8px] min-w-[180px] pr-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#1E2B3A] text-white hover:[linear-gradient(0deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1)), #0D2247] no-underline flex gap-x-2  active:scale-95 scale-100 duration-75"
                 style={{
@@ -118,7 +118,7 @@ export default function Home() {
               }}
             >
               <Link
-                href="https://app.brstore.us"
+                href="https://app.buildandrun.net"
                 className="group rounded-full px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#f5f7f9] text-[#1E2B3A] no-underline active:scale-95 scale-100 duration-75"
                 style={{
                   boxShadow: "0 1px 1px #0c192714, 0 1px 3px #0c192724",
